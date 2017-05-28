19b45506bde2d585decf61e351846928890ad10e
Valeri26 <valerich_lera@list.ru>
2017-05-28 13:29:31 +0300

    Add fastSearch

---
+++ fastSearch.php
@@ -0,0 +1,43 @@
+﻿<?php
+	$host='localhost';
+	$user='root';
+	$password='';
+	$dbname='search_squad_results';
+	
+	//session_start();
+	header('Content-Type: text/xml');
+	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+	echo '<response>';
+	
+	$db=mysqli_connect($host,$user,$password,$dbname);
+	mysqli_set_charset($db, "utf8");
+	if ($db->connect_errno) {
+		echo '</response>';
+		exit;
+	}
+	
+	$searchRow = $_GET["searchRow"];
+	
+	$query = "select first_name,second_name,third_name,date_format(birth_date,(select description from dateformats where id=birthDateFormat_id)) as bdate,
+	(select name from warriorranks where id=rank_id) as rank from warriors where concat(first_name,' ',second_name,' ', third_name)=?;";
+	
+	$stmt = $db->prepare($query);
+	$stmt->bind_param("s",$searchRow);
+	
+	$stmt->execute();
+	
+	$result = $stmt->get_result();
+	while ($row = $result->fetch_assoc()) {
+			echo "<warrior>";
+			echo "<first_name>" . $row["first_name"] . "</first_name>";
+			echo "<second_name>" . $row["second_name"] . "</second_name>";
+			echo "<third_name>" . $row["third_name"] . "</third_name>";
+			echo "<bdate>" . $row["bdate"] . "</bdate>";
+			echo "<rank>" . $row["rank"] . "</rank>";			
+			echo "</warrior>";		
+	}
+	
+	$stmt->close();
+	$db->close();
+	echo '</response>';
+?>
--- index.php
+++ index.php
@@ -66,6 +66,106 @@ function signOut() {
 <link href="eCommerceAssets/styles/eCommerceStyle.css" rel="stylesheet" type="text/css">
 <!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
 <script>var __adobewebfontsappname__="dreamweaver"</script><script src="http://use.edgefonts.net/montserrat:n4:default;source-sans-pro:n2:default.js" type="text/javascript"></script>
+
+<script> 
+	var xmlHttp = createXmlHttpRequestObject();
+	
+	function createXmlHttpRequestObject() {
+		var xmlHttp;
+		if (window.ActiveXObject) {
+			try {
+				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				xmlHttp = false;
+			}
+		}
+		else
+		{
+			try {
+				xmlHttp = new XMLHttpRequest();
+			} catch (e) {
+				xmlHttp = false;
+			}
+		}
+		if (!xmlHttp) {
+		alert("Error while creation object XMLHttpRequest") }
+		 else {
+			return xmlHttp;
+		}
+	}
+	
+
+	function FastSearch() {	
+		var txtFastSearch = document.getElementById("txtFastSearch");
+		var searchRow = encodeURIComponent(txtFastSearch.value);
+		
+		if (xmlHttp.readyState == 4 || xmlHttp.readyState == 0) {
+			xmlHttp.open("GET", "fastSearch.php?searchRow="+searchRow, true);
+			xmlHttp.onreadystatechange = handleServerResponse;
+			xmlHttp.send(null);	
+		}
+	}
+	
+	function handleServerResponse()
+	{
+		if (xmlHttp.readyState == 4)
+		{
+			if (xmlHttp.status == 200)
+			{
+				xmlResponse = xmlHttp.responseXML;	
+				xmlRoot = xmlResponse.documentElement;
+				
+				warriors = xmlRoot.getElementsByTagName("warrior");
+				
+				
+				
+				
+				
+				var htmlCode = "<table style='border: 1px solid black;  border-collapse: collapse; width: 100%;'>";
+				
+				htmlCode += "<tr><th style='border: 1px solid black;'>Фамилия</th><th style='border: 1px solid black;'>Имя</th><th style='border: 1px solid black;'>Отчество</th><th style='border: 1px solid black;'>Дата рождения</th><th style='border: 1px solid black;'>Звание</th></tr>";
+				
+				for( var i = 0; i < warriors.length; i++) {
+					htmlCode += "<tr>";
+					
+					var children = warriors.item(i).childNodes;
+					
+					for( var j = 0; j < children.length; j++) {
+						htmlCode += "<td style='border: 1px solid black;'>";	
+						htmlCode += children.item(j).innerHTML;
+						htmlCode += "</td>";
+					}
+					htmlCode += "</tr>";
+				}
+				
+				htmlCode += "</table>"
+				
+				 var element = document.getElementById("mContent");
+				 element.innerHTML = htmlCode;
+			}
+		}
+	}
+	
+	function addRowHandlers() {
+		var table = document.getElementById("tableId");
+		var rows = table.getElementsByTagName("tr");
+		for (i = 0; i < rows.length; i++) {
+			var currentRow = table.rows[i];
+			var createClickHandler = 
+				function(row) 
+				{
+					return function() { 
+											var cell = row.getElementsByTagName("td")[0];
+											var id = cell.innerHTML;
+											alert("id:" + id);
+									 };
+				};
+
+			currentRow.onclick = createClickHandler(currentRow);
+		}
+	}	
+</script>
+
 </head>
 
 <body>
@@ -107,8 +207,13 @@ function signOut() {
 
  
  <center>
-<input type="text"  id="txtFastSearch" placeholder="ФИО бойца">
-<input type="button" name="btnFastSearch" id="button" value="Найти">
+ 
+ 
+<input type="text"  id="txtFastSearch" placeholder="ФИО бойца" value="Андреев Алексей Иванович">
+<input type="button" name="btnFastSearch" id="button" value="Найти" onclick="FastSearch();">
+
+
+
  </center>
  
   
@@ -124,7 +229,7 @@ function signOut() {
             <!-- List of links under menuset 1 -->
             <li><a href="#" title="Link">О проекте</a></li>
             <li><a href="http://lenww2.ru/" title="Link">Мемориалы</a></li>
-            <li><a href="#" title="Link">Поисковые отряды/объединения</a></li>
+            <li><a href="http://rf-poisk.ru/region/47/reestr/" title="Link">Поисковые отряды/объединения</a></li>
             <li class="notimp"><!-- notimp class is applied to remove this link from the tablet and phone views --><a href="#"  title="Link">Расширенный поиск</a></li>
             <li><a href="#" title="Link">Внести данные</a></li>
             <li><a href="#" title="Link">Обратная связь</a></li>
@@ -134,7 +239,7 @@ function signOut() {
     </section>
     <section class="mainContent">
 
-	<div>
+	<div id="mContent">
 	
 	</div>
 	
